9d3d0f41d8c241e3e1306c88501f3a3f88f4b417
BK -40 <bkhadiza277@gmail.com>
2021-08-07 13:26:22 +0600

    math buildin funtion

---
+++ mathFunction.js
@@ -0,0 +1,17 @@
+const Number1 = -3;
+const output = Math.abs(Number1);
+const arr = [12.098 , 4.675,7.98 ,12];
+console.log("Absolute number of -5 is :",output);
+console.log("Celling value of 12.098 is : ", Math.ceil(arr[0]));
+console.log("Floor value of 4.675  is :", Math.floor(arr[1]));
+console.log("Round value of 7.08 is : ", Math.round(arr[2]));
+console.log("\nRandom Number: ");
+
+
+var i = 1;
+ do{
+let randomNumber = Math.round(Math.random()*6);     
+console.log(randomNumber);
+i++;
+    
+}while (i<10);
